500b53a99e3cb75b516a6f534e11b0c21e2b266e
Marknix <116298439+Marknix666@users.noreply.github.com>
2023-03-01 21:22:50 +0300

    change commit

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BonusServiceTest {
     @Test
-    void shouldCalculateForRegisteredAndUnderLimit() {
+    public void shouldCalculateForRegisteredAndUnderLimit() {
         BonusService service = new BonusService();
 
         long amount = 1000;
@@ -31,4 +31,34 @@ public class BonusServiceTest {
 
         assertEquals(expected, actual);
     }
+
+    @Test
+    public void shouldCalculateUnregisteredAndUnderLimit() {
+        BonusService service = new BonusService();
+
+        long amount = 5000;
+        boolean registered = false;
+        long expected = 50;
+
+
+        long actual = service.calculate(amount, registered);
+
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void shouldCalculateUnregisteredAndOverLimit() {
+        BonusService service = new BonusService();
+
+
+        long amount = 1_000_000;
+        boolean registered = true;
+        long expected = 500;
+
+
+        long actual = service.calculate(amount, registered);
+
+        assertEquals(expected, actual);
+    }
 }
+
